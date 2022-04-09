2fe2746639ec0e1829b84759c99e9805c4c29cd9
noodleween <happyguy122x@gmail.com>
2022-04-08 19:42:55 -0600

    CB-9 Refactor: modularize application_env_vars.sh

--- application_env_vars.sh
+++ application_env_vars.sh
@@ -1,3 +1,4 @@
+export MICROSERVICE=user
 export APP_PORT=8070
 export DB_USERNAME=root
 export DB_PASSWORD=root
@@ -6,5 +7,7 @@ export DB_PORT=3306
 export DB_NAME=alinedb
 export JWT_SECRET_KEY='WmZq4t7w!z%C&F)J@NcRfUjXn2r5u8x/'
 echo "Exports have been applied"
-echo "Running user microservice"
-mvn spring-boot:run -pl user-microservice/
+echo "Updating ${MICROSERVICE} microservice"
+mvn clean install -U
+echo "Running ${MICROSERVICE} microservice"
+mvn spring-boot:run -pl ${MICROSERVICE}-microservice/
